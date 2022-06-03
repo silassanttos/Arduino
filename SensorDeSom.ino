0d48799d7c19a5ff3b8a24260578f53450632817
Silas Dos Santos Sousa <69328711+silassanttos@users.noreply.github.com>
2022-06-02 22:43:08 -0300

    Create SensorDeSom.ino

---
+++ SensorDeSom.ino
@@ -0,0 +1,40 @@
+//DEFINICAO DOS PINOS
+#define pinLed1  2
+#define pinLed2  3
+#define pinLed3  4
+#define pinLed4  5
+#define pinLed5  6
+#define pinLed6  7
+#define pinLed7  8
+#define pinLed8  9
+#define pinLed9  10
+#define pinLed10 11
+
+
+void setup() {
+  pinMode(pinLed1, OUTPUT);
+  pinMode(pinLed2, OUTPUT);
+  pinMode(pinLed3, OUTPUT);
+  pinMode(pinLed4, OUTPUT);
+  pinMode(pinLed5, OUTPUT);
+  pinMode(pinLed6, OUTPUT);
+  pinMode(pinLed7, OUTPUT);
+  pinMode(pinLed8, OUTPUT);
+  pinMode(pinLed9, OUTPUT);
+  pinMode(pinLed10, OUTPUT);
+}
+
+void loop() {
+  int valorLido = analogRead(A0);
+
+  digitalWrite(pinLed1, valorLido > 0); 
+  digitalWrite(pinLed2, valorLido > 20); 
+  digitalWrite(pinLed3, valorLido > 40); 
+  digitalWrite(pinLed4, valorLido > 60); 
+  digitalWrite(pinLed5, valorLido > 80); 
+  digitalWrite(pinLed6, valorLido > 100); 
+  digitalWrite(pinLed7, valorLido > 120); 
+  digitalWrite(pinLed8, valorLido > 140); 
+  digitalWrite(pinLed9, valorLido > 160); 
+  digitalWrite(pinLed10, valorLido > 180); 
+}
